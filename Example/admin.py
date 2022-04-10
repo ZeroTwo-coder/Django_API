4114a824b5dfd770919f4c9b58ca0de16e941469
ZeroTwo-coder <metenov_artem@bk.ru>
2022-04-10 20:50:33 +0300

    TODO: end admin.py and add serializers.py

--- Example/admin.py
+++ Example/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Example
 
-# Register your models here.
+
+class ExampleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'descriptions', 'date')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'descriptions')
+    # list_editable = 'title'  # Edit without log in the model
+    list_filter = 'date'
+
+
+admin.register(Example, ExampleAdmin)
