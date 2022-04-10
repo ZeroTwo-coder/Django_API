ec4aca2284340dff10fa2daa8d233f4d4d3f56a1
ZeroTwo-coder <metenov_artem@bk.ru>
2022-04-10 20:05:30 +0300

    Create Model

--- Example/models.py
+++ Example/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Example(models.Model):
+    title = models.CharField(max_length=150)
+    descriptions = models.CharField(max_length=300, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
